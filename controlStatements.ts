bdf981eb21c0971264612532560ea20fe601d5de
unknown <raminisai33@gmail.com>
2019-08-23 10:00:00 +0530

    control statements

---
+++ controlStatements.ts
@@ -0,0 +1,15 @@
+var nums = [1,2,3,4,5]
+console.log(1 in nums)
+
+// for each(var item in items){}
+
+nums.forEach(element =>{
+    console.log(element)
+})
+
+console.log("using for")
+
+for (let x in nums){
+    console.log(x);
+}
+
